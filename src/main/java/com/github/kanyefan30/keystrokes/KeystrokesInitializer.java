8a92e104fd66da52ecfdd537286dfd96fe45ec9d
KanyeFan30 <matthewkirkland049@gmai.com>
2024-09-20 23:56:24 +1000

    Added @Mod tag

--- src/main/java/com/github/kanyefan30/keystrokes/KeystrokesInitializer.java
+++ src/main/java/com/github/kanyefan30/keystrokes/KeystrokesInitializer.java
@@ -4,6 +4,7 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
+@Mod(modid = "keystrokes", useMetadata = true)
 public class KeystrokesInitializer {
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
