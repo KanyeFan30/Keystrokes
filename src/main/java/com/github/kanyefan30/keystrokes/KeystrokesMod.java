3fbaa76488cd99fefb5a72dd97a180021724821d
KanyeFan30 <matthewkirkland049@gmai.com>
2024-09-21 10:23:40 +1000

    Different keys now have unique box widths

--- src/main/java/com/github/kanyefan30/keystrokes/KeystrokesMod.java
+++ src/main/java/com/github/kanyefan30/keystrokes/KeystrokesMod.java
@@ -5,14 +5,16 @@ import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.ScaledResolution;
 import net.minecraft.client.settings.KeyBinding;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+import java.util.Objects;
 
 public class KeystrokesMod extends Gui {
     private final Minecraft mc = Minecraft.getMinecraft();
-    private static final int BOX_WIDTH = 20;
+    private static final int KEY_WIDTH = 20;
+    private static final int SPACE_WIDTH = 60;
+    private static final int MB_WIDTH = 30;
+    private static final int BUTTON_HEIGHT = 20;
 
     @SubscribeEvent
     public void onRenderGameOverlay(RenderGameOverlayEvent.Text event) {
@@ -20,15 +22,15 @@ public class KeystrokesMod extends Gui {
         int screenWidth = sr.getScaledWidth();
         int screenHeight = sr.getScaledHeight();
 
-        int baseX = screenWidth - 2 * BOX_WIDTH;
-        int baseY = screenHeight - 4 * BOX_WIDTH;
-        renderKey(baseX, baseY, "W", mc.gameSettings.keyBindForward);
-        renderKey(baseX - BOX_WIDTH, baseY + BOX_WIDTH, "A", mc.gameSettings.keyBindLeft);
-        renderKey(baseX, baseY + BOX_WIDTH, "S", mc.gameSettings.keyBindBack);
-        renderKey(baseX + BOX_WIDTH, baseY + BOX_WIDTH, "D", mc.gameSettings.keyBindRight);
-        renderKey(baseX, baseY + 2 * BOX_WIDTH, "—", mc.gameSettings.keyBindJump);
-        renderKey(baseX - BOX_WIDTH, baseY + 3 * BOX_WIDTH, "LMB", mc.gameSettings.keyBindAttack);
-        renderKey(baseX + BOX_WIDTH, baseY + 3 * BOX_WIDTH, "RMB", mc.gameSettings.keyBindUseItem);
+        int baseX = screenWidth - 3 * KEY_WIDTH;
+        int baseY = screenHeight - 4 * KEY_WIDTH;
+        renderKey(baseX + KEY_WIDTH, baseY, "W", mc.gameSettings.keyBindForward);
+        renderKey(baseX, baseY + BUTTON_HEIGHT, "A", mc.gameSettings.keyBindLeft);
+        renderKey(baseX + KEY_WIDTH, baseY + KEY_WIDTH, "S", mc.gameSettings.keyBindBack);
+        renderKey(baseX + 2 * KEY_WIDTH, baseY + BUTTON_HEIGHT, "D", mc.gameSettings.keyBindRight);
+        renderKey(baseX, baseY + 2 * BUTTON_HEIGHT, "-", mc.gameSettings.keyBindJump);
+        renderKey(baseX, baseY + 3 * BUTTON_HEIGHT, "LMB", mc.gameSettings.keyBindAttack);
+        renderKey(baseX + MB_WIDTH, baseY + 3 * BUTTON_HEIGHT, "RMB", mc.gameSettings.keyBindUseItem);
     }
 
     private void renderKey(int x, int y, String key, KeyBinding keyBinding) {
@@ -39,9 +41,20 @@ public class KeystrokesMod extends Gui {
             keyColour = 0xFF000000;
         }
 
-        drawRect(x, y, x + BOX_WIDTH, y + BOX_WIDTH, boxColour);
         int letterWidth = mc.fontRendererObj.getStringWidth(key);
         int letterHeight = mc.fontRendererObj.FONT_HEIGHT;
-        mc.fontRendererObj.drawString(key, x + (BOX_WIDTH - letterWidth) / 2, y + (BOX_WIDTH - letterHeight) / 2, keyColour);
+        int boxWidth = getBoxWidth(key);
+        drawRect(x, y, x + boxWidth, y + BUTTON_HEIGHT, boxColour);
+        mc.fontRendererObj.drawString(key, x + (boxWidth - letterWidth) / 2, y + (BUTTON_HEIGHT - letterHeight) / 2, keyColour);
+    }
+
+    private int getBoxWidth(String key) {
+        if (Objects.equals(key, "-")) {
+            return SPACE_WIDTH;
+        } else if (Objects.equals(key, "LMB") || Objects.equals(key, "RMB")) {
+            return MB_WIDTH;
+        } else {
+            return KEY_WIDTH;
+        }
     }
 }
